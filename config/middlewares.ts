11335218e5b330953f2ee5f90b15ca1fe94b743d
Kavindu Theekshana <kavindutheekshana@gmail.com>
2025-11-14 00:24:34 +0000

    Backend

---
+++ config/middlewares.ts
@@ -0,0 +1,34 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://darshanagalketayam.lk', 'https://api.darshanagalketayam.lk'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://darshanagalketayam.lk', 'https://api.darshanagalketayam.lk'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:3000', 'https://darshanagalketayam.lk', 'https://api.darshanagalketayam.lk'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
